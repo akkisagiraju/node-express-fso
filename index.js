72ef1c2741b6c6b80b01bb4ee56812fc9aea89a2
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-03-22 11:20:23 +0530

    edited PORT

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.get('/info', (req, res) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
