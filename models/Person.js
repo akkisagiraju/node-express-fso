17efa2512ff6987b0d83cc43d7e37f33e789085b
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-03-23 03:34:20 +0530

    minlength added

--- models/Person.js
+++ models/Person.js
@@ -14,11 +14,13 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 3,
     unique: true
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    minlength: 8
   },
   id: Number
 });
